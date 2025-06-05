a9dcad529ac1ea1ce6359f90e589451109b1ab3f
Hitesh kanodia <hiteshkanodia34@gmail.com>
2025-06-05 23:11:31 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,12 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import { fileURLToPath } from "url";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
-import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  base: "/PersonalPortflio/",
+  base: "/PersonalPortfolio/",
   plugins: [
     react(),
     runtimeErrorOverlay(),
